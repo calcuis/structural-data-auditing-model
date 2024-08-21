ac3a78f0646e613aa8abd65305d3a092ff2d8b81
calcuis <113646141+calcuis@users.noreply.github.com>
2024-08-22 06:58:17 +0800

    Update unmatched.py

--- unmatched.py
+++ unmatched.py
@@ -8,18 +8,18 @@ def find_unmatched():
             if file.endswith('.csv'):
                 file_path = os.path.join(root, file)
                 try:
-                    df = pd.read_csv(file_path, usecols=[0, 1, 4, 5, 6])
+                    df = pd.read_csv(file_path, usecols=[0, 1])
                 except Exception as e:
                     print(f"Could not read {file_path}: {e}")
                     continue
-                df.columns = ['Session', 'Timestamp', 'Sender', 'Receiver', 'Receiver_ID']
+                df.columns = ['Time', 'Score']
                 df.dropna(inplace=True)
                 df['Source'] = file
-                df = df[['Source', 'Session', 'Timestamp', 'Sender', 'Receiver', 'Receiver_ID']]
+                df = df[['Source', 'Time', 'Score']]
                 result.append(df)
     if result:
         combined_df = pd.concat(result)
-        duplicates = combined_df.duplicated(subset=['Session', 'Timestamp', 'Sender', 'Receiver', 'Receiver_ID'], keep=False)
+        duplicates = combined_df.duplicated(subset=['Time', 'Score'], keep=False)
         unique_df = combined_df[~duplicates]
         unique_df.to_csv('output.csv', index=False)
     else:
